ad039ebe55676c5847bba6dadc5d81c914740eeb
Daniel Brandenburg <danbburg@gmail.com>
2025-03-08 16:41:07 -0500

    start a built in node list

---
+++ src/BuiltInNodes.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let nodes = [];
+
+    nodes.push({
+        name: 'variable',
+        inputs: [
+            {name: 'value', type: 'any', default: 0}
+        ],
+        outputs: [
+            {name: 'value', type: 'number', value: 0}
+        ],
+        source: ''
+    });
+    nodes.push(
+        {
+            name: 'add',
+            inputs: [
+                {name: 'a', type: 'number', default: 0},
+                {name: 'b', type: 'number', default: 0}
+            ],
+            outputs: [
+                {name: 'value', type: 'number', value: 0}
+            ],
+            sourceTemplate: 'f"value = {a} + {b}"'
+        }
+    )
+
+</script>
